a3eeef536081ed6982bb94f21fe7bba1c4a1b3fb
Ryan Gasparini <ryan@rxgx.com>
2019-08-14 20:09:05 -0700

    File cleanup and settings change (#17)
    * move files
    
    * remove items and update config
    
    * rename workflow
    
    * rename workflow and add include

---
+++ src/withGraphData.test.ts
@@ -0,0 +1,37 @@
+import { isDataLoading, hasDataError, withGraphLoading } from "./withGraphData";
+
+describe("isDataLoading", () => {
+  test("returns false when loading", () => {
+    expect(
+      isDataLoading({
+        data: { error: undefined, loading: false, items: undefined }
+      })
+    ).toBe(false);
+  });
+
+  test("returns true when loaded", () => {
+    expect(
+      isDataLoading({
+        data: { error: undefined, loading: true, items: undefined }
+      })
+    ).toBe(true);
+  });
+});
+
+describe("hasDataError", () => {
+  test("returns false without error", () => {
+    expect(
+      hasDataError({
+        data: { error: undefined, loading: true, items: undefined }
+      })
+    ).toBe(false);
+  });
+
+  test("returns true with error", () => {
+    expect(
+      hasDataError({
+        data: { error: Symbol("error"), loading: true, items: undefined }
+      })
+    ).toBe(true);
+  });
+});
---
+++ src/withGraphData.ts
@@ -0,0 +1,39 @@
+import { branch, compose, renderComponent } from "recompose";
+
+interface Props {
+  data: {
+    loading?: boolean;
+    [name: string]: any;
+    error?: any;
+  };
+}
+
+export function hasDataError(props: Props): boolean {
+  return Boolean(props.data.error);
+}
+
+export function isDataLoading(props: Props): boolean {
+  return Boolean(props.data.loading);
+}
+
+export function withGraphLoading(
+  LoadingComponent: React.ComponentClass | React.FunctionComponent | string
+) {
+  return branch(isDataLoading, renderComponent(LoadingComponent));
+}
+
+export function withGraphError(
+  ErrorComponent: React.ComponentClass | React.FunctionComponent | string
+) {
+  return branch(hasDataError, renderComponent(ErrorComponent));
+}
+
+export default function withGraphData(
+  LoadingComponent: any,
+  ErrorComponent: any
+) {
+  return compose(
+    withGraphLoading(LoadingComponent),
+    withGraphError(ErrorComponent)
+  );
+}
